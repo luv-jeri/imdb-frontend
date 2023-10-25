ddb7f7b89b3bfff1136ad83300e3b097d3ac0901
Sanjay Kumar Gautam <sanjay.kumar@climateconnect.digital>
2023-10-25 22:05:28 +0530

    Added-Logo in menu component

--- src/components/layout/Menu/Menu.jsx
+++ src/components/layout/Menu/Menu.jsx
@@ -8,6 +8,15 @@ const Menu = ({ showMenu, style, className, handleClick }) => {
         transform: showMenu ? "translateY(0)" : "translateY(-150%)",
       }}
     >
+      <img
+        src="/public/logo.png"
+        alt="logo"
+        className={s.logo}
+        style={{
+          height: "6rem",
+          width: "10rem",
+        }}
+      />
       <Button height="3rem" onClick={handleClick}>
         Close
       </Button>
